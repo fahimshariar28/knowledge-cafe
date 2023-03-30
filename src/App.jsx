97d98e5a03194263731d9eb78476f2bd52603a2a
Fahim Shariar <fahimshariar28@gmail.com>
2023-03-30 22:36:23 +0600

    Header added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App w-10/12 mx-auto">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
