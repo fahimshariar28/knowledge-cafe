a37720fde48014fc48e751bbbd337bb0134a58e0
Fahim Shariar <fahimshariar28@gmail.com>
2023-03-31 02:42:03 +0600

    Post Section and side bar section sized

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,8 @@ import Posts from "../Posts/Posts";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-2 gap-3">
-      <div>
+    <div className="grid grid-cols-3 gap-3">
+      <div className="col-span-2">
         <Posts></Posts>
       </div>
       <div>
