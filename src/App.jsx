2a1b93e0770063db28096711443a3eb0db9cfdfc
Fahim Shariar <fahimshariar28@gmail.com>
2023-03-31 00:52:06 +0600

    Posts Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
-import Header from "./Header/Header";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App w-10/12 mx-auto">
       <Header></Header>
+      <hr className="my-3" />
+      <Home></Home>
     </div>
   );
 }
