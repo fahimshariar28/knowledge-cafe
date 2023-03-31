17df4fe8c9339635ae8694e8e6ec1f3a74d53426
Fahim Shariar <fahimshariar28@gmail.com>
2023-03-31 23:39:17 +0600

    Ready to Launch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,16 @@ const Header = () => {
   return (
     <div className="mt-3 flex justify-between items-center">
       <h1 className="text-3xl font-bold">My Blog</h1>
-      <img
-        className="rounded-[50%] w-12"
-        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-        alt=""
-      />
+      <div className="flex flex-row justify-center items-center gap-3">
+        <a href="#home">Home</a>
+        <a href="#about">About Us</a>
+        <a href="#contact">Contact</a>
+        <img
+          className="rounded-[50%] w-12"
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
